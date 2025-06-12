6d815525a090a773fde160b69b82fd95221044fd
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-06-11 17:06:34 -0700

    Update and rename Closures.groovy to Closures_and_ScriptStyleGroovy.groovy

--- Closures_and_ScriptStyleGroovy.groovy
+++ Closures_and_ScriptStyleGroovy.groovy
@@ -14,3 +14,10 @@ class Demo {
 // in groovy class is not necessary
 
 println 'Oleg is in Groovy now!'
+
+//
+// Script-style code: Groovy allows writing code
+// in a script-style without wrapping it in a class, which is common for simple tasks. You can run code directly in a Groovy script.
+def x = 5
+def X = 6
+println("The value of x is $x and X is $X")
