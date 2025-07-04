c8641e5549419906504a84fe220ac57e9b211784
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-07-03 20:32:09 -0700

    Add files via upload

---
+++ a19_Inheritence.groovy
@@ -0,0 +1,11 @@
+class a19_Inheritence {
+    String amidablefriendly;
+    String bello_pretty;
+
+    def read2() {
+        println "$amidablefriendly reads"
+    }
+    def write2(){
+        println "$bello_pretty reading writing it"
+    }
+}
