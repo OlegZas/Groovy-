c997740f7be0882711144c9491a3094bae2d38b6
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-07-03 20:32:39 -0700

    Add files via upload

---
+++ c19_Inheritence.groovy
@@ -0,0 +1,13 @@
+class c19_Inheritence {
+
+    static void main(args){
+        b19_Inheritence child = new b19_Inheritence()
+
+        child.amidablefriendly = "oleg"
+        child.bello_pretty = 'reading here practice yes yes'
+        child.write2()
+        child.read2()
+        child.pobreza = "poverty"
+        child.juradoJudge()
+    }
+}
