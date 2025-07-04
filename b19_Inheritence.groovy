d30ae693e476f83aceb85f02471d3d8455fc5a40
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-07-03 20:32:29 -0700

    Add files via upload

---
+++ b19_Inheritence.groovy
@@ -0,0 +1,6 @@
+class b19_Inheritence extends a19_Inheritence{ // extends means that it's a child class of the a19. it will inherit it's methods
+    def pobreza;
+    def juradoJudge(){
+        println("$pobreza printing more variables")
+    }
+}
