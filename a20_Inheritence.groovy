b4b272b90536d9cff6f456c6f913c04b58c26560
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-07-04 19:03:22 -0700

    Add files via upload

---
+++ a20_Inheritence.groovy
@@ -0,0 +1,19 @@
+class a20_Inheritence {
+    // this would be the parent class
+    // the properties in this class are shared by the children
+    String venture;
+    String wonky;
+    String precarious;
+
+    // function that is common to all
+    def brazen (){
+        println ("brazen means obvious and bold.$venture means to go on some risky journey; also, noun - risky journey")
+    }
+    def facetious (){
+        println("facetious means making fun of something not appropriate, some serious issue.While $wonky means crooked or offcenter")
+        }
+
+    def compel () {
+        println("compel means to force to do somethin. $precarious means unstable or uncertain ")
+    }
+}
