fe8f9107ee5fb93255effe604d6f222eca8255cb
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-07-08 09:03:05 -0700

    Add files via upload

---
+++ c22_InterfaceClass.groovy
@@ -0,0 +1,18 @@
+class c22_InterfaceClass implements a22_Interfaceclass {
+    def notion(){
+        print("idea or thought or belief\n")
+    };
+    def cellar(){
+        print("basement\n")
+    };
+    def quiver(){
+        print("to shake and shiver Oleg")
+    };
+
+    static void main(args) {
+        def g = new c22_InterfaceClass()
+        g.notion();
+        g.cellar();
+        g.quiver();
+    }
+}
